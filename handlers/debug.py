0f5b47489e1b6d6e34cc7523fefe2bd448969594
KojieKO <kojie90@gmail.com>
2024-05-21 12:16:42 +0200

    debug_stop

--- handlers/debug.py
+++ handlers/debug.py
@@ -3,17 +3,16 @@ from telegram.ext import CallbackContext
 import requests
 import os
 
-GITHUB_REPO = os.getenv('REPO')  # Utiliza el secreto REPO
+GITHUB_REPO = os.getenv('REPO')
 GITHUB_TOKEN = os.getenv('GH_TOKEN')
 
-def cancel_workflow_run():
+def set_stop_command_received():
     url = f"https://api.github.com/repos/{GITHUB_REPO}/actions/runs"
     headers = {
         "Authorization": f"token {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
     }
     response = requests.get(url, headers=headers)
-    print(f"GET {url} - Status Code: {response.status_code}")
     if response.status_code == 200:
         workflows = response.json().get('workflow_runs', [])
         for workflow in workflows:
@@ -21,9 +20,8 @@ def cancel_workflow_run():
                 run_id = workflow['id']
                 cancel_url = f"https://api.github.com/repos/{GITHUB_REPO}/actions/runs/{run_id}/cancel"
                 cancel_response = requests.post(cancel_url, headers=headers)
-                print(f"POST {cancel_url} - Status Code: {cancel_response.status_code}")
                 if cancel_response.status_code == 202:
-                    print(f"Workflow {run_id} cancelado con éxito.")
+                    os.environ['STOP_COMMAND_RECEIVED'] = 'true'
                     return True
                 else:
                     print(f"Error al cancelar el workflow {run_id}: {cancel_response.status_code}")
@@ -36,7 +34,7 @@ def debug_stop(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Bot detenido por emergencia. ¡Hasta luego!")
     context.bot_data['updater'].stop()
     context.bot_data['updater'].is_idle = False
-    if cancel_workflow_run():
+    if set_stop_command_received():
         update.message.reply_text("Workflow cancelado con éxito.")
     else:
         update.message.reply_text("No se pudo cancelar el workflow o no había ningún workflow en progreso.")
